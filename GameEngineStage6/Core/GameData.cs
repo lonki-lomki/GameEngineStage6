38ea8ece5a14fe1d92e0cb632238e82e532296b9
Дмитрий Карташов <cit.utc@gmail.com>
2017-03-12 23:02:54 +0200

    В проект добавлен NLua. Начало наполнения классами, реализующими движок.

---
+++ GameEngineStage6/Core/GameData.cs
@@ -0,0 +1,37 @@
+﻿using GameEngineStage6.Utils;
+using System;
+using System.Reflection;
+
+namespace GameEngineStage6.Core
+{
+    class GameData
+    {
+        // Набор значений для определения текущего состояния игры
+        public enum GameState
+        {
+            NotStarted,
+            MainMenu,
+            Level,
+            LevelWin,
+            GameWin,
+            GameOver
+        }
+
+        public PhysWorld world;
+
+        public Assembly myAssembly;
+
+        public GameState currentGameState = GameState.Level;
+
+        public int score = 0;
+
+        public Random rnd = new Random();
+
+        public Logger log;
+
+        public ResourceManager rm;
+
+        /////////////////////////////////////////////////////////
+
+    }
+}
--- GameEngineStage6/Form1.cs
+++ GameEngineStage6/Form1.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameEngineStage6.Core;
+using System;
 using System.Windows.Forms;
 
 namespace GameEngineStage6
 {
     public partial class Form1 : Form
     {
+
+        private Timer timer = new Timer();
+
+        // Счётчик количества тиков
+        private long tickCount = 0;
+        // Для определения длины интервала времени в тиках
+        private long saveTickCount = 0;
+
+        /// <summary>
+        /// Игровые данные
+        /// </summary>
+        private GameData gd;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +27,13 @@ namespace GameEngineStage6
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            /* Полноэкранный режим
             this.Width = Screen.PrimaryScreen.WorkingArea.Width;
             this.Height = Screen.PrimaryScreen.WorkingArea.Height;
             this.TopMost = true;
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
+            */
         }
     }
 }
