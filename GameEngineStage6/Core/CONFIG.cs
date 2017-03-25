2cba4636dbb29438bd278d264c9adae728c78ac2
Дмитрий Карташов <cit.utc@gmail.com>
2017-03-25 23:12:01 +0200

    Новая карта (больше, чем может вместить экран). Отображение части карты. Возможность попиксельного смещения камеры вдоль карты.

--- GameEngineStage6/Core/CONFIG.cs
+++ GameEngineStage6/Core/CONFIG.cs
@@ -10,14 +10,14 @@
         public static readonly int START_X = 20;
         public static readonly int START_Y = 20;
 
+        public static readonly int VIEWPORT_WIDTH = 800;
+        public static readonly int VIEWPORT_HEIGHT = 512;
+
         // Размер тайла
-        public static readonly int TILE_SIZE = 64;
+        public static readonly int TILE_SIZE = 32;
 
         public static readonly float PHYS_GRAVITY = 1.1f; //5.0f; // Гравитация для физ. движка
 
         public static readonly float MAX_ENG_POWER = 5.0f;  // Максимальная мощность двигателя
-
-        //public static readonly float timeBetweenWaves = 25.0f;  // Время между волнами
-        //public static readonly float timeBetweenMob = 1.0f;     // Время между монстрами внутри волны
     }
 }
--- GameEngineStage6/Entities/TiledMapObject.cs
+++ GameEngineStage6/Entities/TiledMapObject.cs
@@ -12,6 +12,13 @@ namespace GameEngineStage6.Entities
     {
         public Map map;
 
+        /// <summary>
+        /// Размеры камеры
+        /// </summary>
+        private Rectangle viewPort = new Rectangle(16, 16, CONFIG.VIEWPORT_WIDTH, CONFIG.VIEWPORT_HEIGHT);
+
+        private Bitmap image;
+
         public TiledMapObject() : base()
         {
         }
@@ -27,6 +34,21 @@ namespace GameEngineStage6.Entities
         public TiledMapObject(String id, GameData gd, Map map) : base(id, gd)
         {
             this.map = map;
+            image = new Bitmap(map.Layers["Layer 1"].Width * CONFIG.TILE_SIZE, map.Layers["Layer 1"].Height * CONFIG.TILE_SIZE);
+            Graphics g = Graphics.FromImage(image);
+
+            // Нарисовать всю карту на виртуальном холсте
+            for (int j = 0; j < map.Layers["Layer 1"].Height; j++)
+            {
+                for (int i = 0; i < map.Layers["Layer 1"].Width; i++)
+                {
+                    int tileCode = map.Layers["Layer 1"].Tiles[i + j * map.Layers["Layer 1"].Width];
+                    if (tileCode > 0)
+                    {
+                        g.DrawImage(gd.rm.GetImage("tileset-" + tileCode), i * gd.ss.GetTileWidth(), j * gd.ss.GetTileHeight(), gd.ss.GetTileWidth(), gd.ss.GetTileHeight());
+                    }
+                }
+            }
         }
 
         public override void Render(Graphics g)
@@ -43,9 +65,10 @@ namespace GameEngineStage6.Entities
             //map.Layers[i].Height - высота уровня в тайлах
 
             // Имя файла с тайлами
-            int ccc = map.Tilesets.Count;
+            //int ccc = map.Tilesets.Count;
 
-            // Цикл по строкам
+            /*
+            // Циклы по ячейкам
             for (int j = 0; j < map.Layers["Layer 1"].Height; j++)
             {
                 for (int i = 0; i < map.Layers["Layer 1"].Width; i++)
@@ -60,6 +83,47 @@ namespace GameEngineStage6.Entities
                 }
             }
             //g.DrawString(s.Substring(i, 1), new Font("Arial", 12), Brushes.Black, position.X + i * 12, position.Y + j * 12);
+            */
+
+            /*
+            // Предварительный расчет параметров для циклов
+            int startTileX = viewPort.X / CONFIG.TILE_SIZE; // Начальный тайл (левый верхний)
+            int startTileY = viewPort.Y / CONFIG.TILE_SIZE;
+            int endTileX = startTileX + viewPort.Width / CONFIG.TILE_SIZE;   // Конечный тайл (правый нижний)
+            int endTileY = startTileY + viewPort.Height / CONFIG.TILE_SIZE;
+
+            // Проверка выхода за пределы диапазона тайловой карты
+            if (startTileX < 0) startTileX = 0;
+            if (startTileY < 0) startTileY = 0;
+            if (endTileX > map.Layers["Layer 1"].Width) endTileX = map.Layers["Layer 1"].Width;
+            if (endTileY > map.Layers["Layer 1"].Height) endTileY = map.Layers["Layer 1"].Height;
+            */
+
+            // TODO: вывод части тайла 
+            // position.X - координата в окне, откуда начинать выводить карту
+            // viewPort.X - координата тайловой карты, начиная с которой надо начинать выводить карту
+
+            // Цикл вывода тайлов, которые попадают в область видимости камеры
+            /*
+            for (int j = startTileY, jj = 0; j < endTileY; j++, jj++)
+            {
+                for (int i = startTileX, ii = 0; i < endTileX; i++, ii++)
+                {
+                    int tileCode = map.Layers["Layer 1"].Tiles[i + j * map.Layers["Layer 1"].Width];
+                    if (tileCode > 0)
+                    {
+                        g.DrawImage(gd.rm.GetImage("tileset-" + tileCode), position.X + ii * gd.ss.GetTileWidth(), position.Y + jj * gd.ss.GetTileHeight(), gd.ss.GetTileWidth(), gd.ss.GetTileHeight());
+                    }
+                }
+            }
+            */
+
+            // Вывести часть игрового поля, на которую указывает viewPort
+            g.DrawImage(image, new Rectangle(CONFIG.START_X, CONFIG.START_Y, viewPort.Width, viewPort.Height), viewPort, GraphicsUnit.Pixel);
+
+            // Нарисовать границы области видимости игрового поля
+            g.DrawRectangle(Pens.LightGreen, CONFIG.START_X, CONFIG.START_Y, viewPort.Width, viewPort.Height);
+
         }
     }
 }
